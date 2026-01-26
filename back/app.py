9053cdcb8090b062a00b7fec2f1a6d37bdad7e6f
baptmnt <bapt.menot@gmail.com>
2026-01-26 16:52:47 +0100

    Add Flask application setup with SQLAlchemy and initial route

---
+++ back/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+from config import Config
+# Import models BEFORE db initialization to register metadata
+from models import *
+from models.db import db
+
+
+
+import logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+
+# Initialiser l'application Flask
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Initialiser SQLAlchemy
+db.init_app(app)
+
+
+
+# Routes
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Bienvenue sur Flask avec PostgreSQL et SQLAlchemy!'})
+
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        print("Created")
+    app.run(debug=True)
