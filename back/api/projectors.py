84ed57f5cdfff64502a51453fbbde61b66c17f34
Baptiste <bapt.menot@gmail.com>
2026-01-27 15:14:10 +0100

    Add optional filtering for projectors by is_led status

--- back/api/projectors.py
+++ back/api/projectors.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from models.projector import Projector
 from . import api_bp
@@ -6,5 +6,13 @@ from . import api_bp
 
 @api_bp.get("/projectors")
 def list_projectors():
-    projectors = Projector.query.all()
+    query = Projector.query
+    
+    # Filtre optionnel par is_led
+    is_led = request.args.get('is_led')
+    if is_led is not None:
+        is_led_bool = is_led.lower() in ('true', '1', 'yes')
+        query = query.filter_by(is_led=is_led_bool)
+    
+    projectors = query.all()
     return jsonify([p.to_dict() for p in projectors])
