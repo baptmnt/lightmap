d055018e3d52d89034b38f440ab779d626aeddc5
baptmnt <bapt.menot@gmail.com>
2026-02-02 08:56:18 +0100

    add openapi documentation and docs exposure

--- back/app.py
+++ back/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from config import Config
 # Import models BEFORE db initialization to register metadata
 from models import *
 from models.db import db
 from api import api_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+from pathlib import Path
 
 
 
@@ -29,6 +31,28 @@ app.register_blueprint(api_bp)
 def hello():
     return jsonify({'message': 'Bienvenue sur Flask avec PostgreSQL et SQLAlchemy!'})
 
+# Route pour servir le fichier OpenAPI
+@app.route('/api/v1/openapi.yaml', methods=['GET'])
+def openapi_spec():
+    openapi_path = Path(__file__).parent / 'docs/openapi.yaml'
+    return send_file(openapi_path, mimetype='text/yaml')
+
+# Configuration Swagger UI
+SWAGGER_URL = '/api/v1/docs'  # URL for exposing Swagger UI
+API_URL = '/api/v1/openapi.yaml'  # Our API url
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Lightmap API",
+        'layout': 'BaseLayout',
+        'deepLinking': True
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 
 
